9c9f29e4df8f197d59a82a9b1a1ce077edce5d2c
Veselin Denchev <den4ev01@gmail.com>
2023-05-07 14:49:31 +0300

    Create main menu base
    Create MainMenu scene with header (with custom font) and buttons each with attached script method

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Play()
+    {
+        // Load main map scene
+    }
+
+    public void Options()
+    {
+        // Options button click logic
+    }
+
+    public void Quit() => Application.Quit();
+}
