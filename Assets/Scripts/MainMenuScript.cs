7a6a31c6c2b81b75b4812f903aa48c5be6c73000
Veselin Denchev <den4ev01@gmail.com>
2023-05-08 13:20:25 +0300

    Add controls screen in the main menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -3,12 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject mainMenuScreen;
+    public GameObject controlsScreen;
+    public GameObject rulesScreen;
+
     public void Play() => SceneManager.LoadScene("Game");
 
-    public void Rules()
+    public void Controls() => this.HideMainMenuAndShowAnotherScreen(this.controlsScreen);
+
+    public void Rules() => this.HideMainMenuAndShowAnotherScreen(this.rulesScreen);
+
+    public void BackToMainMenuScreen()
     {
-        // Rules button click logic
+        this.controlsScreen.SetActive(false);
+        this.rulesScreen.SetActive(false);
+        this.mainMenuScreen.SetActive(true);
     }
 
     public void Quit() => Application.Quit();
+
+    private void HideMainMenuAndShowAnotherScreen(GameObject otherScreen)
+    {
+        this.mainMenuScreen.SetActive(false);
+        otherScreen.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (!this.mainMenuScreen.activeSelf && Input.GetKey(KeyCode.Escape))
+        {
+            this.BackToMainMenuScreen();
+        }
+    }
 }
